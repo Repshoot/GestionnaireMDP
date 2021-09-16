6fdee3a43430ad9cee21a318e87347fde5f8e411
Guillaume <sk_8_397@hotmail.com>
2021-09-15 22:43:36 -0400

    tooltip ok

--- view/viewPasswords.php
+++ view/viewPasswords.php
@@ -13,15 +13,50 @@ body {
     <tr><th>Site</th><th>Identifiant</th><th>Mot de passe</th></tr>
         <?php
         while ($ligneInfos = $requetePsw->fetch()) {
-            echo '<tr>
-                <td>'. $ligneInfos['website'].'</td> <td>'. $ligneInfos['username'].'</td> <td>'.$ligneInfos['password'].'</td>
-            </tr>';
-         
+            ?><tr>
+                <td><?= $ligneInfos['website'] ?> </td> <td> <?= $ligneInfos['username'] ?></td> <td> <span id="<?='psw'. $ligneInfos['id']?>"><?= $ligneInfos['password'] ?></span></td><td>
+                    <div class="tooltip">    
+                        <a class="copy-psw" onclick="copyClipboard('<?= 'psw'. $ligneInfos['id'] ?>')" onmouseout="outFunc('<?= 'psw'. $ligneInfos['id'] ?>')">
+                                <img class="img-clipboard" src="../public/assets/clipboard.png">
+                                <span>Copier le mdp</span>
+                                <span class="tooltiptext" id="myTooltip<?='psw'. $ligneInfos['id']?>"></span>
+                            </a>
+                    </div>
+                </td>
+            </tr>
+            <?php
         }
         ?>
     </table>
 </div>
 
+<script>
+
+function copyClipboard(pswToCopy) {
+
+    var id = "#" + pswToCopy;
+    var copyText = document.querySelector(id);
+
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    textArea.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand("Copy");
+    textArea.remove();
+
+    var toolTipId =  "myTooltip"+pswToCopy;
+    var tooltip = document.getElementById(toolTipId);
+    tooltip.innerHTML = "Mot de passe copié !";
+}
+
+function outFunc(pswToCopy) {
+    var toolTipId =  "myTooltip"+pswToCopy;
+    var tooltip = document.getElementById(toolTipId);
+    tooltip.innerHTML = "Copier dans le presse papier";
+}
+
+</script>
 
 <?php
     $content = ob_get_clean();
